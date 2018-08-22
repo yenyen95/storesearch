04002b3fcfb5d00e284eeafd1a38838c9a85a875
Aurélien <a.schneberger@gmail.com>
2018-08-22 11:41:00 +0200

    Add a search bar and table view. The search puts fake results in the table for now.

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,12 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Aurélien Schneberger on 22/08/2018.
+//  Copyright © 2018 Aurélien Schneberger. All rights reserved.
+//
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
