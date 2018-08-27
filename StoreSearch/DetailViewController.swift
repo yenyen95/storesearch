2cddf1b1c9ae94a5412f6112bc8ca1479550c482
Aurélien <a.schneberger@gmail.com>
2018-08-27 11:59:25 +0200

    DetailViewController Step 1

---
+++ StoreSearch/DetailViewController.swift
@@ -0,0 +1,76 @@
+//
+//  DetailViewController.swift
+//  StoreSearch
+//
+//  Created by Aurélien Schneberger on 27/08/2018.
+//  Copyright © 2018 Aurélien Schneberger. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    @IBOutlet weak var popupView: UIView!
+    @IBOutlet weak var artworkImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var artistNameLabel: UILabel!
+    @IBOutlet weak var kindLabel: UILabel!
+    @IBOutlet weak var genreLabel: UILabel!
+    @IBOutlet weak var priceButton: UIButton!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        modalPresentationStyle = .custom
+        transitioningDelegate = self
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.tintColor = UIColor(red: 20/255, green: 160/255, blue: 160/255, alpha: 1)
+        popupView.layer.cornerRadius = 10
+        
+        let gestureRecognizer = UITapGestureRecognizer(target: self,
+                                                       action: #selector(close))
+        gestureRecognizer.cancelsTouchesInView = false
+        gestureRecognizer.delegate = self
+        view.addGestureRecognizer(gestureRecognizer)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    
+    // MARK:- Actions
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
+
+extension DetailViewController: UIViewControllerTransitioningDelegate {
+    
+    func presentationController(forPresented presented: UIViewController, presenting: UIViewController?, source: UIViewController) -> UIPresentationController? {
+        return DimmingPresentationController(presentedViewController: presented, presenting: presenting)
+    }
+}
+
+extension DetailViewController: UIGestureRecognizerDelegate {
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
+        return (touch.view === self.view)
+    }
+}
---
+++ StoreSearch/DimmingPresentationController.swift
@@ -0,0 +1,14 @@
+//
+//  DimmingPresentationController.swift
+//  StoreSearch
+//
+//  Created by Aurélien Schneberger on 27/08/2018.
+//  Copyright © 2018 Aurélien Schneberger. All rights reserved.
+//
+
+import UIKit
+class DimmingPresentationController: UIPresentationController {
+    override var shouldRemovePresentersView: Bool {
+        return false
+    }
+}
